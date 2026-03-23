5ce0a0d7954307e04be156292cfc19f20a7ff2b5
Prasun <prasundwivedi9@gmail.com>
2026-03-23 23:42:03 +0530

    factrioal of n using recursion

---
+++ recursion_factorial.py
@@ -0,0 +1,8 @@
+def fact(n):
+    if n == 0 or n==1:
+        return 1
+    return n * fact(n-1)
+
+num = int(input("Enter the number to find the factorial\n"))
+res = fact(num)
+print("Factorial of", num, " =", res)
